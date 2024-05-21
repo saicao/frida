38ff2ea8e2b84e1448f4ebd7c0314370f34ed93e
Sai Cao <1665673333@qq.com>
2024-05-21 11:04:16 +0800

    update build sdk

--- build_sdk.sh
+++ build_sdk.sh
@@ -1,7 +1,10 @@
 
-FRIDA_HOST=ios-arm64
+export FRIDA_HOST=ios-arm64
 make -f Makefile.sdk.mk
  
 FRIDA_HOST=macos-arm64 
 make -f Makefile.sdk.mk
-cp build/sdk-macos-arm64e.tar.bz2 build/sdk-ios-arm64.tar.bz2 sdk
+
+FRIDA_HOST=macos-arm64e 
+make -f Makefile.sdk.mk
+cp build/sdk-macos-arm64.tar.bz2 build/sdk-macos-arm64e.tar.bz2  build/sdk-ios-arm64.tar.bz2  local_sdk
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 make core-ios  
 echo "run scp" 
-scp -P 2222 build/frida-ios-arm64/usr/lib/frida/frida-agent.dylib  root@172.20.10.11:/var/jb/usr/lib/frida
+scp -P 2222 build/frida-ios-arm64/usr/lib/frida/frida-agent.dylib  root@192.168.43.121:/var/jb/usr/lib/frida
 # scp  build/frida-ios-arm64/usr/lib/frida/frida-agent.dylib  root@my-iphone:/var/jb/usr/lib/frida
+# export MACOS_CERTID=frida-cert                                                                                                                                    ─╯
+#export IOS_CERTID=frida-cert
+#export WATCHOS_CERTID=frida-cert
+#export TVOS_CERTID=frida-cert
