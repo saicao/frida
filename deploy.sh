2cfc27eeddc2add7733beedd2001b00863709fd9
Sai Cao <1665673333@qq.com>
2025-01-14 17:16:46 +0800

    remove local sdk

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 make core-ios  
 echo "run scp" 
-scp -P 2222 build/frida-ios-arm64/usr/lib/frida/frida-agent.dylib  root@172.20.10.11:/var/jb/usr/lib/frida
+#  为什么我的codesign 签名出来的东西无法执行？？？？用不了呢？？？？？
+ldid -Sfrida-core/server/frida-server.xcent build/frida-ios-arm64/usr/bin/frida-server
+# scp -P 2222 -r build/frida-ios-arm64  root@192.168.43.162:/var/jb/var/root
+scp -r build/frida-ios-arm64  my-iphone:/var/jb/var/root
 # scp  build/frida-ios-arm64/usr/lib/frida/frida-agent.dylib  root@my-iphone:/var/jb/usr/lib/frida
